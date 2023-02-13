e9cbe31f3276ee0f7a23dd192ef30913834e4ac7
Jay Ives <austin.jay.ives@gmail.com>
2023-02-12 18:14:59 -0800

    Finish CH2

--- stack-underflow/src/main.rs
+++ stack-underflow/src/main.rs
@@ -1,12 +1,20 @@
-use stack_underflow::models::question::{Question, QuestionId, Tag};
-use std::str::FromStr;
+use std::net::SocketAddr;
 
-fn main() {
-    let question = Question::new(
-        QuestionId::from_str("1").expect("No id provided"),
-        "First Question".to_string(),
-        "Content of question".to_string(),
-        Some(vec![Tag::from_str("faq").expect("No tag provided")]),
-    );
-    println!("{}", question);
+use axum::{extract::Path, routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/hello/:name", get(hello_route));
+
+    // run our app with hyper
+    // `axum::Server` is a re-export of `hyper::Server`
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn hello_route(Path(name): Path<String>) -> String {
+    format!("Hello {}", name)
 }
